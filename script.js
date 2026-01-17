50eac37444719258470de99a0bb8595ef28d2804
Wehlia <wehliakahin@gmail.com>
2026-01-17 19:21:21 +0000

    Added styling to CSS file and styling to JS file

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//Creating a second header in JS
-const newHeader = document.createElement("h2");
+const elementToStyle = document.getElementById("sign-up-link");
 
-newHeader.textContent = "This is our Header 2";
-
-document.body.appendChild(newHeader);
-
-// Step 1. Find Elements
-const changeText = document.getElementById("change-text");
-const hideText = document.getElementById("hide-text");
-
-// Step 2. Modify Elements
-changeText.textContent = "Text Changed";
-
-// Step 3. Remove Elements
-hideText.remove();
+elementToStyle.style.color = "purple";
